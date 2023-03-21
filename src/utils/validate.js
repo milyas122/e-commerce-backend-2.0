28dc7195a71219a5f533e6b82a248b35714d373f
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-22 00:56:46 +0500

    Validations: add auth schema

---
+++ src/utils/validate.js
@@ -0,0 +1,10 @@
+const { ValidationError } = require("yup");
+
+module.exports = async function (schema, fields) {
+  try {
+    return await schema.validate(fields, { stripUnknown: true });
+  } catch (e) {
+    // console.log(e);
+    throw new ValidationError(e.errors.shift());
+  }
+};
---
+++ src/utils/validations/auth.js
@@ -0,0 +1,29 @@
+const { object, string, boolean, addMethod } = require("yup");
+
+function checkPassword(message) {
+  return this.test("isPasswordMatched", message, function (value) {
+    const { path, createError, parent } = this;
+
+    if (value !== parent.password) {
+      return createError({ path, message: message ?? "Password not matched" });
+    }
+    return true;
+  });
+}
+
+addMethod(string, "isPasswordMatched", checkPassword);
+
+const signupSchema = object({
+  name: string().required(),
+  email: string().email().required(),
+  password: string().min(5).max(20).required(),
+  confirmPassword: string().min(5).max(20).isPasswordMatched().required(),
+  isSeller: boolean().default(false),
+});
+
+const loginSchema = object({
+  email: string().email().required(),
+  password: string().required(),
+});
+
+module.exports = { signupSchema, loginSchema };
