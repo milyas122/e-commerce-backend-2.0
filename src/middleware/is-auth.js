379308a10f950211c6b579285537732399ebc044
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-26 12:42:08 +0500

    Middleware: is-auth

---
+++ src/middleware/is-auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async function (req, res, next) {
+  try {
+    const token = req.headers["x-auth-token"] || req.headers["authorization"];
+    if (!token) {
+      return res
+        .status(404)
+        .json({ message: "Access denied - No token provided" });
+    }
+    // console.log(process)
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.log(err);
+    if (err.name === "JsonWebTokenError") {
+      return res.status(404).json({ message: "Provide a valid token" });
+    }
+    return res.status(404).json({ message: "Internal Server Error" });
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/user");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUser);
+router.get("/:id", isAuth, userController.getUser);
 
 module.exports = router;
